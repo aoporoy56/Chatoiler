557eab24faf7daf36b4d6abe2633735372c057a0
aoporoy <aopo56@gmail.com>
2023-04-24 02:49:23 +0600

    Mongo URL

--- backend/config/mongo_connection.js
+++ backend/config/mongo_connection.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 exports.connectDB = async () =>{
     try {
-        await mongoose.connect(process.env.MONGO_DB);
+        await mongoose.connect(
+          "mongodb+srv://root:EAIhKvVS31J1spin@cluster0.aku2ier.mongodb.net/?retryWrites=true&w=majority"
+        );
         console.log("Database Connected");  
     } catch (error) { 
         console.log(error.message); 
