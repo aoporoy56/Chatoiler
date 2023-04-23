652f199e23d239b2e33170fcbf8a81040d271c96
aoporoy <aopo56@gmail.com>
2023-04-24 03:02:16 +0600

    .env

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const UserRouter = require("./routes/UserRoute");
 const ChatRoute = require("./routes/ChatRoute");
 const MessgeRoute = require("./routes/MessageRoute");
 const path = require("path");
-dotenv.config({path : "../.env"});
+dotenv.config({path : path.join(__dirname+"../.env")});
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
